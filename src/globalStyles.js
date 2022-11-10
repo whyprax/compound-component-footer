a7670c1cb86cedadcf416bce49a5154b1b65330b
prathamdubey <prathamd67@gmail.com>
2022-11-11 01:39:08 +0530

    Global Styles Added

---
+++ src/globalStyles.js
@@ -0,0 +1,8 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyles = createGlobalStyle`
+html, body{
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+}`;
